14aaa85642f958f12b7593719fff124f6ff4f13c
cosmindev <cosmin.tudor82@gmail.com>
2022-05-16 21:25:23 +0300

    Add to the input parameters

--- variables.tf
+++ variables.tf
@@ -1,10 +1,7 @@
 # Copyright (c) 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-variable "default_compartment_id" {
-  type        = string
-  description = "The default compartment OCID to use for resources (unless otherwise specified)."
-}
+
 
 variable "instance_pool_config" {
   type = object({
@@ -42,6 +39,8 @@ variable "instance_pool_config" {
         defined_tags   = map(string),
         freeform_tags  = map(string),
         display_name   = map(string),
+        instance_id    = string,
+        source         = string,
         instance_id    = string,
         source         = string,
 
@@ -82,15 +81,23 @@ variable "instance_pool_config" {
 
           launch_details = object({
 
-            ad                      = string,
-            fd = string,
-            capacity_reservation_id = string,
-            compartment_id          = string,
-            dedicated_vm_host_id    = string,
-            defined_tags            = map(string),
-            freeform_tags           = map(string),
-            display_name = string,
-            extended_metadata = map(string)
+            ad                                  = string,
+            fd                                  = string,
+            capacity_reservation_id             = string,
+            compartment_id                      = string,
+            dedicated_vm_host_id                = string,
+            defined_tags                        = map(string),
+            freeform_tags                       = map(string),
+            display_name                        = string,
+            extended_metadata                   = map(string)
+            ipxe_script                         = string,
+            is_pv_encryption_in_transit_enabled = bool,
+            launch_mode                         = string,
+            metadata                            = map(string),
+            preferred_maintenance_action        = string,
+            shape                               = string,
+
+
 
             agent_config = object({
               are_all_plugins_disabled = bool,
@@ -120,94 +127,71 @@ variable "instance_pool_config" {
               subnet_id                 = string,
             })
 
+            instance_options = object({
+              are_legacy_imds_endpoints_disabled = bool
+            })
+
+            launch_options = object({
+              boot_volume_type                    = string,
+              firmware                            = string,
+              is_consistent_volume_naming_enabled = bool,
+              is_pv_encryption_in_transit_enabled = bool,
+              network_type                        = string,
+              remote_data_volume_type             = string
+            })
+
+            platform_config = object({
+              type                               = string,
+              is_measured_boot_enabled           = bool,
+              is_secure_boot_enabled             = bool,
+              is_trusted_platform_module_enabled = bool,
+              numa_nodes_per_socket              = number
+            })
+
+            preemptible_instance_config = object({
+              preemption_action = object({
+                type                 = string
+                preserve_boot_volume = bool
+              })
+            })
+
+            shape_config = object({
+              baseline_ocpu_utilization = string,
+              memory_in_gbs             = number,
+              nvmes                     = number,
+              ocpus                     = number
+            })
+
+            source_details = object({
+              source_type             = string,
+              boot_volume_id          = string,
+              boot_volume_size_in_gbs = number,
+              image_id                = number
+            })
+
             ##### instance_options
+          })
+
+          secondary_vnics = object({
+            display_name = string,
+            nic_index    = number,
+
+            create_vnic_details = object({
+              assign_private_dns_record = bool,
+              assign_public_ip          = bool,
+              defined_tags              = map(string),
+              freeform_tags             = map(string),
+              display_name              = map(string),
+              hostname_label            = string,
+              nsg_ids                   = list(string),
+              private_ip                = string,
+              skip_source_dest_check    = bool,
+              subnet_id                 = string
+            })
           })
 
         })
       }))
     })
   })
-}
-
-
-# Instance variables
-variable "instance_configuration" {
-  type = object(
-    map(object({
-      ad             = number,
-      compartment_id = string,
-      shape          = string,
-
-      is_monitoring_disabled = bool,
-
-      subnet_id           = string,
-      assign_public_ip    = bool,
-      vnic_defined_tags   = map(string),
-      vnic_display_name   = string,
-      vnic_freeform_tags  = map(string),
-      nsg_ids             = list(string),
-      private_ip          = string,
-      skip_src_dest_check = bool,
-
-      defined_tags          = map(string),
-      extended_metadata     = map(string),
-      fault_domain          = string,
-      freeform_tags         = map(string),
-      hostname_label        = string,
-      ipxe_script           = string,
-      pv_encr_trans_enabled = bool,
-
-      ssh_authorized_keys    = list(string),
-      ssh_private_keys       = list(string),
-      bastion_ip             = string,
-      user_data              = string,
-      image_name             = string,
-      mkp_image_name         = string,
-      mkp_image_name_version = string,
-      source_id              = string,
-      source_type            = string,
-      boot_vol_size_gbs      = number,
-      kms_key_id             = string,
-
-      preserve_boot_volume = bool,
-      instance_timeout     = string,
-
-      sec_vnics = map(object({
-        #Required
-        subnet_id = string,
-
-        #Optional
-        assign_public_ip    = bool,
-        defined_tags        = map(string),
-        vnic_display_name   = string,
-        freeform_tags       = map(string),
-        hostname_label      = string,
-        nsg_ids             = list(string),
-        private_ip          = string,
-        skip_src_dest_check = bool,
-
-        #Required
-        instance_id = string,
-
-        #Optional
-        display_name = string,
-        nic_index    = number
-      })),
-
-      mount_blk_vols = bool
-      block_volumes = list(object({
-        volume_id        = string,
-        attachment_type  = string,
-        volume_mount_dir = string
-      })),
-      cons_conn_create    = bool,
-      cons_conn_def_tags  = map(string),
-      cons_conn_free_tags = map(string),
-
-  })))
-
-  description = "Parameters for each instance pool"
-
-  default = null
-
 }
