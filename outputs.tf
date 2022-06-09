1dc032a3ab3d04b3ada010fc3d4e417ddb14c16f
cosmindev <cosmin.tudor82@gmail.com>
2022-06-09 06:26:49 +0300

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -4,10 +4,9 @@
 output "instance_pool" {
   description = "The instance pool and instance configuration details"
   value = {
-    instance_configuration = oci_core_instance_configuration.instance_configuration
-    instance_pool          = oci_core_instance_pool.instance_pool,
-    auto_scaling           = oci_autoscaling_auto_scaling_configuration.auto_scaling_configuration
-    local_load_balancers   = local.load_balancers
+    instance_configuration = oci_core_instance_configuration.instance_configuration[0]
+    instance_pool          = oci_core_instance_pool.instance_pool[0],
+    auto_scaling           = oci_autoscaling_auto_scaling_configuration.auto_scaling_configuration[0]
   }
 }
 
